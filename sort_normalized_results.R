6878aeb0d2d3439db2ad852e8a49a4301e3787b5
Patricia Sieber <46709466+patriciasieber@users.noreply.github.com>
2020-01-07 16:50:39 +0100

    Update sort_normalized_results.R

--- sort_normalized_results.R
+++ sort_normalized_results.R
@@ -4,24 +4,16 @@ library(gdata)
 library(ggplot2)
 library(gridExtra)
 
-setwd("/data/Postdoc/miRNA_targets/ProjectSummary/Data/") # ran on local machine
+setwd("/data/PmiRNA_targets/ProjectSummary/Data/") # ran on local machine
 
-results <- read.csv("normalized_dcq_values_ref_20653.csv",sep="\t",header=T)
+results <- read.csv("normalized_values.csv",sep="\t",header=T)
 
 sampleSelection <- read.xls("../../data/sampleSelection.xlsx",sheet=2)
 sampleSelection <- sampleSelection[,1:7]
 
 ## sort for interesting candidate genes
-mirnas <- c("hsa-miR-204-5p","hsa-miR-132-3p","hsa-miR-9-5p")
-dems <- c(## DE miRNAs male
-          "hsa-miR-194-5p","hsa-miR-376a-3p","hsa-miR-215-5p","hsa-miR-495-3p","hsa-miR-382-5p","hsa-miR-151a-5p",  
-          "hsa-miR-182-5p","hsa-miR-151a-3p","hsa-miR-30d-5p","hsa-miR-23b-3p","hsa-miR-33a-5p","hsa-miR-223-3p",
-          "hsa-miR-766-3p","hsa-miR-324-3p","hsa-miR-340-5p","hsa-miR-27b-3p","hsa-miR-339-5p","hsa-miR-409-3p",
-          "hsa-miR-154-5p","hsa-miR-192-5p",
-          ## DE miRNAs female
-          "hsa-miR-21-5p","hsa-miR-342-3p","hsa-miR-484","hsa-miR-150-5p","hsa-miR-191-5p","ssa-miR-103a-3p","hsa-miR-181a-5p",
-          "hsa-miR-222-3p","hsa-miR-17-5p","hsa-miR-328-3p","hsa-miR-106a-5p","hsa-miR-155-5p","hsa-miR-20a-5p","hsa-let-7d-5p",
-          "hsa-miR-18b-5p","hsa-miR-15b-5p","hsa-miR-30e-5p","hsa-miR-107","hsa-miR-140-3p","hsa-miR-663a")
+mirnas <- NULL ## TODO: fill!
+dems <- NULL ## TODO: fill!
 
 mirnas_pos <- unlist(lapply(mirnas,function(x){grep(x,results$dilution_id)}))
 dems_pos <- unlist(lapply(dems,function(x){grep(x,results$dilution_id)}))
@@ -34,8 +26,8 @@ results <- results[c(1:3,mirnas_pos,dems_pos,rest_pos),]
 dilution_name <- as.character(unlist(lapply(results[1,],as.character)))
 patient_name <- dilution_name[-1]
 
-association_table <- data.frame(patient=c("P043","P010","P055","P015"),associated_control=c("P018","None","P063","P053"))
-
+association_table <- data.frame() ## TODO: fill!
+ 
 
 ## sort for patients in new data.frame:
 ## patient_id; timepoint; case; association; patient_name; miRNAs...
@@ -75,17 +67,17 @@ for(mi in 1:length(mir_names)){
 
 mirnas_patient_table <- mirnas_patient_table[order(mirnas_patient_table$patient_id, mirnas_patient_table$timepoint), ]
 
-write.table(mirnas_patient_table,"normalized_dcq_values_patients.csv",sep="\t",col.names=T,row.names=F,quote=F)
-save(mirnas_patient_table,file="normalized_dcq_values_patients.RData")
+write.table(mirnas_patient_table,"normalized_values_patients.csv",sep="\t",col.names=T,row.names=F,quote=F)
+save(mirnas_patient_table,file="normalized_values_patients.RData")
 
 
 
 
 ## line plots
-plot_mirnas <- c(mirnas,dems) ## c("hsa-miR-204-5p","hsa-miR-132-3p","hsa-miR-9-5p")
+plot_mirnas <- c(mirnas,dems)
 len_mir <- length(plot_mirnas)
 
-plot_patient_pairs <- c("P043_P018","P055_P063","P015_P053","P010_None")
+plot_patient_pairs <- NULL ## TODO: fill!
 len_pat_pairs <- length(plot_patient_pairs)
 yrange <- range(-8,6)
 
